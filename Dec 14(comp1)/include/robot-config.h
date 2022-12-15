34e259e622a000acbf403b3cdc20de5eded02846
JamesPod <83381938+JamesPod@users.noreply.github.com>
2022-12-14 20:09:19 -0500

    Add files via upload

---
+++ Dec 14(comp1)/include/robot-config.h
@@ -0,0 +1,17 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor shooter;
+extern motor pusher;
+extern controller Controller1;
+extern motor shooter2;
+extern motor string1;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
